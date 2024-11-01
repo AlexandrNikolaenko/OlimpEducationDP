00c8b3577e2f3d586f0bf719cfca4993abe820b5
Александр <nikol.alex06@mail.ru>
2024-11-01 08:54:45 +0300

    fix: Chaged formId for AddFileInput for answers

--- olimpeducation/src/app/components/adminPanelCom.js
+++ olimpeducation/src/app/components/adminPanelCom.js
@@ -333,7 +333,6 @@ function AddFileInput({ name, formId, setIsEmpty, accept=".jpg", multiple=false 
             <button className="w-full px-5 py-10 rounded-[10px] bg-transparent border-super-light border-2 border-dashed" onClick={
                 (e) => {
                     e.preventDefault();
-                    console.log('Add file answer');
                     document.getElementById(formId).click();
                 }
             }>{nameFile}</button>
@@ -502,7 +501,7 @@ function AddAnswer() {
                 dataProc('addanswer', 'Ответ успешно добавлен', 'Произошла ошибка', 'Произошла ошибка', e, 'add-answer-form', 'POST', !isEmptyFile);
             }}>
                 <Text>Добавьте файл в формате pdf, названный так, чтобы название оканчивалось номером задания</Text>
-                <AddFileInput name={'Загрузить файл'} formId={'add-answer-form'} setIsEmpty={setIsEmptyFile} accept=".pdf"/>
+                <AddFileInput name={'Загрузить файл'} formId={'inputfile3'} setIsEmpty={setIsEmptyFile} accept=".pdf"/>
                 <SubmitButton color={'bright'} text={'Добавить'}/>
             </form>
         </div>
@@ -517,9 +516,6 @@ function RemoveAnswer() {
             <div className='flex justify-between items-center'>
                 <Text>Удалить ответ</Text>
                 <ArrowButton onClick={() => setIsOpen(!isOpen)} isOpen={isOpen} />
-                {/* <button onClick={() => setIsOpen(!isOpen)} className={`${isOpen ? 'rotate-180' : 'rotate-0'} transition-all duration-200`}>
-                    <Image alt='arrow' src={'/Arrow.svg'} height={43} width={43}/>
-                </button> */}
             </div>
             <form className={`${isOpen ? 'h-auto flex ' : 'h-0 overflow-hidden none'} flex-col gap-y-5 items-start`} id={'remove-answer-form'} onSubmit={function (e){
                 dataProc('removeanswer', 'Ответ успешно удален', 'Произошла ошибка', 'Произошла ошибка', e, 'remove-answer-form', 'DELETE');
@@ -541,16 +537,13 @@ function EditAnswer() {
             <div className='flex justify-between items-center'>
                 <Text>Редактиовать ответ</Text>
                 <ArrowButton onClick={() => setIsOpen(!isOpen)} isOpen={isOpen} />
-                {/* <button onClick={() => setIsOpen(!isOpen)} className={`${isOpen ? 'rotate-180' : 'rotate-0'} transition-all duration-200`}>
-                    <Image alt='arrow' src={'/Arrow.svg'} height={43} width={43}/>
-                </button> */}
             </div>
             <form className={`${isOpen ? 'h-auto flex ' : 'h-0 overflow-hidden none'} flex-col gap-y-5 items-start`} id={'edit-answer-form'} onSubmit={function (e){
                 dataProc('editanswer', 'Ответ успешно отредактирован', 'Произошла ошибка', 'Произошла ошибка', e, 'edit-answer-form', 'POST', !isEmptyFile);
             }}>
                 <Text>Введите ID задания</Text>
                 <OnceInput placeholder={'Task ID'} name={'id'}/>
-                <AddFileInput name={'Загрузить файл'} formId={'edit-answer-form'} accept=".pdf" setIsEmpty={setIsEmptyFile}/>
+                <AddFileInput name={'Загрузить файл'} formId={'inputfile4'} accept=".pdf" setIsEmpty={setIsEmptyFile}/>
                 <SubmitButton color={'bright'} text={'Редактировать'}/>
             </form>
         </div>
