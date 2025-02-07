e93f76d1b16a7a09dcdfc7cf05a22e2668274889
Александр <nikol.alex06@mail.ru>
2025-02-07 22:01:15 +0300

    fix commit

--- olimpeducation/src/app/taskid/[...taskid]/page.js
+++ olimpeducation/src/app/taskid/[...taskid]/page.js
@@ -8,15 +8,28 @@ export default async function Page({params}) {
     let id = params.taskid[0];
 
     if (!isNaN(id)){
-        await fetch(`/api/answer?id=${id}`, {method: 'GET', cache: 'no-cache'})
-                .then(res => res.json()).then(data => answerUrl = data.url);
-    
-        await fetch(`/api/task?id=${id}`, {method: 'GET', cache: 'no-cache'})
-                .then(res => res.json())
-                .then(data => urls = data.url);
+        try {
+            let res1 = await fetch(`/api/answer?id=${id}`, {method: 'GET', cache: 'no-cache'});
+            answerUrl = (await res1.json()).url;
+            // await fetch(`/api/answer?id=${id}`, {method: 'GET', cache: 'no-cache'})
+            //     .then(res => res.json()).then(data => answerUrl = data.url);
+            
+            let res2 = await fetch(`/api/task?id=${id}`, {method: 'GET', cache: 'no-cache'});
+            urls = (await res2.json()).url;
+            // await fetch(`/api/task?id=${id}`, {method: 'GET', cache: 'no-cache'})
+            //     .then(res => res.json())
+            //     .then(data => urls = data.url);
+        } catch(e) {
+            console.log(e);
+        }
+        
     }
-
-    console.log(urls)
+    if (!urls || !answerUrl) {
+        return (
+            <div className="wrapper mx-auto flex flex-col gap-y-[30px] max-[700px]:gap-y-5 px-0 max-[1280px]:px-5">
+            </div>
+        )
+    }
     
     return (
         <div className="wrapper mx-auto flex flex-col gap-y-[30px] max-[700px]:gap-y-5 px-0 max-[1280px]:px-5">
